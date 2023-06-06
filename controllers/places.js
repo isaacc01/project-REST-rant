687a579eaa9eaa2828d5b5bcc67c124fb236013a
Isaac Hernandez <Isaac@Isaacs-Air.attlocal.net>
2023-06-06 14:30:16 -0700

    Adding and rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,10 @@
 const router = require('express').Router()
 
+//Index
+router.get('/new', (req, res) => {
+    res.render('places/new')
+})
+
 router.get('/', (req,res) => {
     let places = [{
         name: 'H-Thai-ML',
