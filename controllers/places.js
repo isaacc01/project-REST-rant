011c72eb5717dfd5ae9147dec5d85ec04f90bc47
Isaac Hernandez <Isaac@Isaacs-Air.attlocal.net>
2023-06-07 22:19:36 -0700

    Adding Bootstrap grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -1,27 +1,23 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
-//Index
-router.get('/new', (req, res) => {
-    res.render('places/new')
-})
+router.get('/', (req, res) => {
+let places = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/thai-food.jpg'
+  }, {
+      name: 'Coding Cafe',
+      city: 'Phoenix',
+      state: 'AZ',
+      cuisines: 'Coffee, Bakery',
+      pic: '/images/coffee-cat.jpg'
+  }]
 
-router.get('/', (req,res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-     }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-     }]
-    
     res.render('places/index', {places})
 })
-
+  
 
 module.exports = router
