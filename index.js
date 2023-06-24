28e38db2b99e5ffa63839cc38aa3200c4252923d
Isaac Hernandez <Isaac@Isaacs-Air.attlocal.net>
2023-06-24 14:20:35 -0700

    secondcommit

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const methodOverride = require('method-override')
+//const methodOverride = require('method-override')
 
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-app.use(methodOverride('_method'))
+//app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 
